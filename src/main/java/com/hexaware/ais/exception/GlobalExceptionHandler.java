e5c014fbcce5f26195dcebc43e601dcd0aa4d604
kishlaykiku <kishlaykumarkkp24@gmail.com>
2024-11-26 18:26:37 +0530

    Minor Fix [Swagger Bean]

--- src/main/java/com/hexaware/ais/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hexaware/ais/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
@@ -16,7 +16,6 @@ import java.time.LocalDateTime;
  * Class: GlobalExceptionHandler
  * Description: This class handles all exceptions thrown by the application
  */
-@ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
